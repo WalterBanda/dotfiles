e8fa0edd82771ab4c8bc9dc147f3d9460e5b0787
Walter Banda <walterkaimurima@gmail.com>
2023-02-23 04:16:56 +0300

    ☕ Updated path to relative

--- profile.d/android.sh
+++ profile.d/android.sh
@@ -2,5 +2,5 @@ export ANDROID_SDK_ROOT=/opt/environments/android-sdk
 export ANDROID_HOME=$ANDROID_SDK_ROOT
 export ANDROID_NDK_HOME=$ANDROID_SDK_ROOT/ndk/25.1.8937393
 
-export PATH="$PATH:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/emulator/"
+export PATH="$PATH:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/emulator"
 
