e5a8d16f352bac731c28d1cb2fbaec92e3ff3f5c
Walter Banda <walterkaimurima@gmail.com>
2023-04-03 19:43:12 +0300

    ☕ bash completion for poetry

--- scripts/poetry_manual.sh
+++ scripts/poetry_manual.sh
@@ -1,2 +1,5 @@
 curl -sSL https://install.python-poetry.org | POETRY_HOME=/opt/environments/poetry python3 -
 
+
+cat " After successful completion, logout and run this to enable bash completions ` poetry completions bash >> /etc/profile.d/bash_completion.sh `"
+
