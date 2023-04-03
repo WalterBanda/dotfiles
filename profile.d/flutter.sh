bac097f8bfb5221f8ef19546c3fb1ed5b110abec
Walter Banda <walterkaimurima@gmail.com>
2023-04-03 19:24:56 +0300

    ☕ Compressing flutter path var

--- profile.d/flutter.sh
+++ profile.d/flutter.sh
@@ -1,5 +1,4 @@
 export FLUTTER_HOME=/opt/environments/flutter
 export DART_SDK=$FLUTTER_HOME/cache/dart-sdk/
-export PATH="$PATH":"$HOME/.pub-cache/bin"
+export PATH="$PATH:$FLUTTER_HOME/bin:$HOME/.pub-cache/bin"
 
-export PATH="$PATH:$FLUTTER_HOME/bin"
