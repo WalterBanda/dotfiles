41ba93c32fdc81257a642d3e4cca5fa95682d501
Walter Banda <walterkaimurima@gmail.com>
2023-06-20 10:44:50 +0300

    Switching to using fvm

--- profile.d/flutter.sh
+++ profile.d/flutter.sh
@@ -1,4 +1,4 @@
-export FLUTTER_HOME=/opt/environments/flutter/global
+export FLUTTER_HOME=/opt/environments/flutter/fvm/default
 export DART_SDK=$FLUTTER_HOME/cache/dart-sdk/
 export PATH="$PATH:$FLUTTER_HOME/bin:$HOME/.pub-cache/bin"
 
